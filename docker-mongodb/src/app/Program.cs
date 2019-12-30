7a647ed83a28af33940cfd1c73f57056621b1610
Maksym Liannoi <liannoi444@gmail.com>
2019-12-30 09:12:48 +0200

    Added an empty console application project

---
+++ docker-mongodb/src/app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Learning.DockerMongoDB.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
